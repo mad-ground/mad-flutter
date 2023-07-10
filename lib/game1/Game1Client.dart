828e8406230a14808cd6fbafd7abdef25201eb0a
retro3014 <36194346+retro3014@users.noreply.github.com>
2023-07-11 02:02:57 +0900

    game1 system init

--- lib/game1/Game1Client.dart
+++ lib/game1/Game1Client.dart
@@ -1,19 +1,129 @@
 import 'package:flutter/material.dart';
 import 'package:madground/component/button.dart';
 import 'package:madground/menu/StartMenu.dart';
+import 'dart:async';
+import 'package:madground/socket/SocketSystem.dart';
+
 
 class Game1System {
-  Game1System();
-
+  Game1System(this.startTimer, this.endTimer, this.reloadState);
+  bool isCurrentUser = true;
   
+  late List<String> userList;
+  int num = 31;
+  Function startTimer, endTimer, reloadState;
+  void setUserList(List<String> userList){
 
+  }
+  void setCurrentUser(String userId){
+    if (userId == "my_user_id"){
+      isCurrentUser = true;
+      startTimer();
+    }else{
+      isCurrentUser = false;
+      startTimer();
+    }
+  }
+
+  Color getTimerColor(){
+    if(isCurrentUser){
+      return Colors.red;
+    }else{
+      return Colors.grey;
+    }
+  }
+
+  void onBtnClick(int btnNumber){
+    if(isCurrentUser){
+      // 서버 연결 시 
+      //SocketSystem.emitMessage("game1_selection", btnNumber);
+      endTimer();
+      // TEST
+      showSelection(btnNumber);
+
+    }
+    // TEST START
+    if(btnNumber==1){
+      setCurrentUser("my_user_id");
+    }else if(btnNumber==2){
+      setCurrentUser("other_user_id");
+    }
+    reloadState();
+    // TEST END
+  }
+
+  int getCurrentNum(){
+    return num;
+  }
+
+  void userInit(List<String> _userList) {
+    
+  }
+
+  int selection = 0;
+  
+  bool isCurrentSelection(int selection){
+    return (this.selection == selection);
+  }
+
+
+  void showSelection(int selection) {
+    this.selection = selection;
+    num -= selection;
+    reloadState();
+  }
+
+  void gameOver(String userId, bool gameEnd) {
+    selection = 0;
+    if(userId == "my_user_id"){
+      // 내 게임 오버 처리
+
+      }else{
+      // 다른 유저 게임 오버 처리
+
+    }
+  }
+
+  void gameEnd() {
+      // 다음 게임으로 넘어가기
+  }
+
+  void setCurrentTurn(String userId, int num){
+    selection = 0;
+    setCurrentUser(userId);
+    this.num = num;
+    reloadState();
+  }
+
+  Color getBtnColor(int btnNum){
+    if(!isCurrentUser && !isCurrentSelection(btnNum)){
+      return Colors.grey;
+    }
+    if(btnNum==1){
+      return Colors.red;
+    }else if(btnNum==2){
+      if(num>=2){
+        return Colors.yellow;
+      }
+      return Colors.grey;
+    }else{
+      if(num>=3){
+        return Colors.green;
+      }
+      else {
+        return Colors.grey;
+      }
+    }
+  }
 }
+
 
 class Game1Home extends StatelessWidget {
   const Game1Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Namer App',
       theme: ThemeData(
@@ -25,8 +135,65 @@ class Game1Home extends StatelessWidget {
   }
 }
 
-class Game1Page extends StatelessWidget {
+class Game1Page extends StatefulWidget {
   const Game1Page({Key? key}) : super(key: key);
+
+  @override
+  State<Game1Page> createState() => _Game1PageState();
+}
+
+class _Game1PageState extends State<Game1Page> {
+  late Timer _timer;
+  int _seconds = 0;
+  bool _isTimerRunning = false;
+  late Game1System game1System;
+  
+  _Game1PageState(){
+    game1System = Game1System(startTimer, endTimer, reloadState);    
+  }
+
+  void startTimer(){
+    const int MAX_TIME = 5;
+    _seconds = MAX_TIME;
+    _isTimerRunning = true;
+    print("TIMER START");
+    print("$_seconds $_isTimerRunning");
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        print("TIMER RUNNING " + "$_seconds");
+        if(!_isTimerRunning){
+          _timer.cancel();
+          _isTimerRunning = false;
+        }
+        if(_seconds>0){
+          _seconds--;
+        }else{
+          _isTimerRunning = false;
+          _timer.cancel();
+          if(game1System.isCurrentUser){
+            game1System.onBtnClick(1);
+          }
+        }
+      });
+    });
+    print(_timer.toString());
+    setState((){});
+  }
+
+  void endTimer(){
+    print("TIMER END");
+    if(_isTimerRunning){
+      _isTimerRunning = false;
+      _timer.cancel();
+    }
+    setState((){});
+  }
+
+
+  void reloadState(){
+    setState((){});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,11 +233,10 @@ class Game1Page extends StatelessWidget {
             },
           ),
         ),
-        const Padding(
+        Padding(
           padding: const EdgeInsets.only(top: 50.0),
-          child: Text(
-            '10',
-            style: TextStyle(color: Colors.red, fontSize: 20),
+          child:Text(
+              "$_seconds", style: TextStyle(color: game1System.getTimerColor(), fontSize: 20),
           ),
         ),
         Padding(
@@ -85,9 +251,9 @@ class Game1Page extends StatelessWidget {
                 borderRadius: BorderRadius.all(Radius.circular(10)),
               ),
               padding: const EdgeInsets.all(16),
-              child: const Text(
-                '31',
-                style: TextStyle(color: Colors.white, fontSize: 50),
+              child: Text(
+                '${game1System.getCurrentNum()}',
+                style: const TextStyle(color: Colors.white, fontSize: 50),
               ),
             ),
           ),
@@ -106,12 +272,17 @@ class Game1Page extends StatelessWidget {
                 ),
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.red, // Set the desired background color
+                    backgroundColor: game1System.getBtnColor(1), // Set the desired background color
                   ),
                   child: const Text('1',
                       style: TextStyle(color: Colors.white, fontSize: 20)),
                   onPressed: () {
                     // Code for button 1 onPressed event
+                    if(game1System.isCurrentUser){
+                      game1System.onBtnClick(1);
+                    }else{
+                      null;
+                    }                    
                   },
                 ),
               ),
@@ -124,12 +295,17 @@ class Game1Page extends StatelessWidget {
                 ),
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.yellow, // Set the desired background color
+                    backgroundColor: game1System.getBtnColor(2), // Set the desired background color
                   ),
                   child: const Text('2',
                       style: TextStyle(color: Colors.black, fontSize: 20)),
                   onPressed: () {
                     // Code for button 2 onPressed event
+                    if(game1System.isCurrentUser && game1System.getCurrentNum()>=2){
+                      game1System.onBtnClick(2);
+                    }else{
+                      null;
+                    }
                   },
                 ),
               ),
@@ -142,12 +318,17 @@ class Game1Page extends StatelessWidget {
                 ),
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.green, // Set the desired background color
+                    backgroundColor: game1System.getBtnColor(3), // Set the desired background color
                   ),
                   child: const Text('3',
                       style: TextStyle(color: Colors.white, fontSize: 20)),
                   onPressed: () {
                     // Code for button 3 onPressed event
+                    if(game1System.isCurrentUser && game1System.getCurrentNum()>=3){
+                      game1System.onBtnClick(3);
+                    }else{
+                      null;
+                    }
                   },
                 ),
               ),
--- lib/socket/SocketSystem.dart
+++ lib/socket/SocketSystem.dart
@@ -1,7 +1,10 @@
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:madground/game1/Game1Client.dart';
+
 
 class SocketSystem{
   static late IO.Socket socket;
+  static late Game1System game1System;
   static void connectServer(){
     
     socket = IO.io('http://172.10.5.147:80',IO.OptionBuilder().setTransports(['websocket']).build());
@@ -17,5 +20,25 @@ class SocketSystem{
     });
     socket.onDisconnect((_) => print('disconnect'));
     socket.on('message', (_) => print(_));
+    
+
+    // Game1System Start
+
+    socket.on('game1_userInit', (data) => game1System.userInit(data["userList"]));
+
+    socket.on('game1_turn', (data) => game1System.setCurrentTurn(data["userId"], data["num"]));
+
+    socket.on("game1_userSelection", (data) => game1System.showSelection(data["selection"]));
+
+    socket.on("game1_gameOver", (data) => game1System.gameOver(data["userId"], data["gameEnd"]));
+
+    socket.on("game1_gameEnd", (data) => game1System.gameEnd());
+
+    // Game1System End
+
+  }
+
+  static void emitMessage(String key, data){
+      socket.emit(key, data);
   }
 }
